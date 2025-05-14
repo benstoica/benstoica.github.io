73d5e3cd342129bba21e1f0b9623350e597170e9
Ben Stoica <bennystoica@gmail.com>
2025-05-13 20:12:14 -0400

    add new project

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import evogym from "../../assets/images/evogym.PNG";
+import mockinterview from "../../assets/images/mockinterview.PNG";
 import { BsGithub, BsArrowRight } from "react-icons/bs";
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
@@ -108,57 +109,107 @@ const Projects = () => {
 
   return (
     <div
-      className='w-full  pb-40 xl:px-44 lg:px-36 md:px-32 sm:px-28 vvs:px-12 px-10'
-      id='projects'>
+      className="w-full pb-40 xl:px-44 lg:px-36 md:px-32 sm:px-28 vvs:px-12 px-10"
+      id="projects">
       <h1
-        id='project-heading'
-        className='text-white/70 font-Glimer-Outlined font-semibold z-40 lg:mb-32 mb-20 vvs:text-[4rem] sm:text-[5.2rem] md:text-[6.4rem] lg:text-[7.5rem] xl:text-[9.2rem] 2xl:text-[10.8rem]  text-[3.5rem] leading-[3.5rem] sm:leading-[5.5rem] md:leading-[6rem] lg:leading-[7rem] xl:leading-[9rem] 2xl:leading-[10rem]  cursor-default'>
+        id="project-heading"
+        className="text-white/70 font-Glimer-Outlined font-semibold z-40 lg:mb-32 mb-20 vvs:text-[4rem] sm:text-[5.2rem] md:text-[6.4rem] lg:text-[7.5rem] xl:text-[9.2rem] 2xl:text-[10.8rem]  text-[3.5rem] leading-[3.5rem] sm:leading-[5.5rem] md:leading-[6rem] lg:leading-[7rem] xl:leading-[9rem] 2xl:leading-[10rem]  cursor-default">
         Featured Projects
       </h1>
-      <div className='sm:space-y-40 space-y-32 xl:space-y-[35vh] z-30'>
-        {/*  */}
-        <div className='container lg:space-x-2 flex flex-col lg:flex-row lg:justify-between lg:items-center items-start invisible'>
-          <div className='lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper'>
-            <a
-              target='_blank'
-              href='https://gym-typescript-eb0.pages.dev/'
-              rel='noreferrer'>
-              <img
-                src={evogym}
-                alt='evo gym'
-                className='absolute w-full object-cover h-full img'
-              />
-            </a>
-            <div className='img-container absolute top-0 right-0 bg-[#420fe7] w-full h-full'></div>
-          </div>
-
-          <div className='text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3'>
-            <h1 className='hidden-text font-bold font-Glimer-Bold cursor-default lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5'>
-              <div className='content-line flex lg:justify-end items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden bg-[#420fe7] '>
-                <div className='content-line-inner'>Evolve Gym</div>
+      <div className="sm:space-y-40 space-y-32 xl:space-y-[35vh] z-30">
+        {/* Interview Prep */}
+        <div className="container lg:space-x-2 flex flex-col lg:flex-row lg:justify-between lg:items-center items-start invisible">
+          <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3">
+            <h1 className="hidden-text font-bold font-Glimer-Bold cursor-default lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
+              <div className="content-line flex lg:justify-start items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden bg-[#420fe7] ">
+                <div className="content-line-inner">Interview Prep</div>
               </div>
             </h1>
-            <p className='font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl  sm:text-xl 2xl:text-3xl vvs:text-base text-sm lg:text-right'>
+            <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl sm:text-xl 2xl:text-3xl vvs:text-base text-sm lg:text-left">
+              A job interview preparation platform that helps you practice your
+              interview skills with real-time feedback. Built with Next.js for
+              the user interface and backend logic, Firebase for authentication
+              and data storage, styled with TailwindCSS and using Vapi AI voice
+              agents with Google Gemini.
+            </p>
+            <div className="btn-row flex justify-start space-x-6 items-center lg:justify-start">
+              <div className="github-btn">
+                <a
+                  className="icon "
+                  target="_blank"
+                  href="https://github.com/benstoica/gym-typescript"
+                  rel="noreferrer">
+                  <BsGithub className="lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200  ease-in-out repo text-white/90 hover:text-white" />
+                </a>
+              </div>
+              <div className="liveDemo-btn">
+                <a
+                  className="icon"
+                  target="_blank"
+                  href="https://gym-typescript-eb0.pages.dev/"
+                  rel="noreferrer">
+                  <BsArrowRight className="link lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-rotate-[45deg] hover:text-white transition duration-200 ease-in-out text-white/90" />
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="lg:h-[250px] w-full relative overflow-hidden img-wrapper">
+            <a
+              target="_blank"
+              href="https://mock-interview-lilac-omega.vercel.app/"
+              rel="noreferrer">
+              <img
+                src={mockinterview}
+                alt="mock interview"
+                className="absolute w-full object-cover h-full img"
+              />
+            </a>
+            <div className="img-container absolute top-0 right-0 bg-[#420fe7] w-full h-full"></div>
+          </div>
+        </div>
+        {/* Evolve Gym */}
+        <div className="container lg:space-x-2 flex flex-col lg:flex-row lg:justify-between lg:items-center items-start invisible">
+          <div className="lg:h-[400px] 2xl:h-[500px] h-[250px] sm:h-[300px] md:h-[350px] w-full relative overflow-hidden img-wrapper">
+            <a
+              target="_blank"
+              href="https://gym-typescript-eb0.pages.dev/"
+              rel="noreferrer">
+              <img
+                src={evogym}
+                alt="evo gym"
+                className="absolute w-full object-cover h-full img"
+              />
+            </a>
+            <div className="img-container absolute top-0 right-0 bg-[#420fe7] w-full h-full"></div>
+          </div>
+
+          <div className="text-container text-white lg:space-y-4 space-y-2 md:space-y-3 sm:space-y-3">
+            <h1 className="hidden-text font-bold font-Glimer-Bold cursor-default lg:text-4xl text-2xl sm:text-3xl 2xl:text-5xl lg:mt-0 mt-5">
+              <div className="content-line flex lg:justify-end items-center m-0 lg:h-[52px] 2xl:h-[60px] overflow-hidden bg-[#420fe7] ">
+                <div className="content-line-inner">Evolve Gym</div>
+              </div>
+            </h1>
+            <p className="font-normal cursor-default font-Glimer-Light text-white/90 xl:text-2xl  sm:text-xl 2xl:text-3xl vvs:text-base text-sm lg:text-right">
               A new type of gym, one that will help you reach your goals like
               never before.
             </p>
-            <div className='btn-row flex justify-start space-x-6 items-center lg:justify-end'>
-              <div className='github-btn'>
+            <div className="btn-row flex justify-start space-x-6 items-center lg:justify-end">
+              <div className="github-btn">
                 <a
-                  className='icon '
-                  target='_blank'
-                  href='https://github.com/benstoica/gym-typescript'
-                  rel='noreferrer'>
-                  <BsGithub className='lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200  ease-in-out repo text-white/90 hover:text-white' />
+                  className="icon "
+                  target="_blank"
+                  href="https://github.com/benstoica/gym-typescript"
+                  rel="noreferrer">
+                  <BsGithub className="lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-translate-y-1 transition duration-200  ease-in-out repo text-white/90 hover:text-white" />
                 </a>
               </div>
-              <div className='liveDemo-btn'>
+              <div className="liveDemo-btn">
                 <a
-                  className='icon'
-                  target='_blank'
-                  href='https://gym-typescript-eb0.pages.dev/'
-                  rel='noreferrer'>
-                  <BsArrowRight className='link lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-rotate-[45deg] hover:text-white transition duration-200 ease-in-out text-white/90' />
+                  className="icon"
+                  target="_blank"
+                  href="https://gym-typescript-eb0.pages.dev/"
+                  rel="noreferrer">
+                  <BsArrowRight className="link lg:h-8 lg:w-8 2xl:h-9 2xl:w-9 vvs:h-6 vvs:w-6 sm:w-7 sm:h-7 h-5 w-5 hover:-rotate-[45deg] hover:text-white transition duration-200 ease-in-out text-white/90" />
                 </a>
               </div>
             </div>
